a1fdb5f9e9cc36e55cc145120be2cf8c12a7ffe0
ujepx64 <natanaeljeffrey@gmail.com>
2024-07-30 10:30:48 +0700

    Video live UI changes
    - dark background
    - youtube desc text inside box, scrollable
    - margin fix
    - added color on colors.xml

--- AndroidStudioProject/app/src/main/java/org/gkisalatiga/plus/screen/ScreenVideoLive.kt
+++ AndroidStudioProject/app/src/main/java/org/gkisalatiga/plus/screen/ScreenVideoLive.kt
@@ -14,12 +14,20 @@ import android.content.ClipData
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.OpenInNew
@@ -40,14 +48,18 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
@@ -77,7 +89,10 @@ class ScreenVideoLive() : ComponentActivity() {
                 ) {
 
             // Display the necessary content.
-            Box ( Modifier.padding(top = it.calculateTopPadding(), bottom = it.calculateBottomPadding()) ) {
+            Box ( modifier= Modifier
+                .padding(top = it.calculateTopPadding(), bottom = it.calculateBottomPadding())
+                .background(color = colorResource(R.color.brown_900))
+                .fillMaxHeight()) {
                 Column {
 
                     // Embedding the YouTube video into the composable.
@@ -96,11 +111,29 @@ class ScreenVideoLive() : ComponentActivity() {
                         )
                         view!!
                     })
-
-                    Text(GlobalSchema.ytViewerParameters["title"]!!, fontWeight = FontWeight.ExtraBold, fontSize = 24.sp)
-                    Text(GlobalSchema.ytViewerParameters["date"]!!)
+                    //jeff 10.25
+                    Spacer(Modifier.height(8.dp))
+                    Text(GlobalSchema.ytViewerParameters["title"]!!,Modifier.absolutePadding(left = 20.dp, right = 20.dp), fontWeight = FontWeight.ExtraBold, fontSize = 24.sp, color = Color.White)
+                    Text(GlobalSchema.ytViewerParameters["date"]!!, Modifier.absolutePadding(left = 20.dp, right = 20.dp), color = Color(0xfffcfcfc))
                     Spacer(Modifier.height(10.dp))
-                    Text(GlobalSchema.ytViewerParameters["desc"]!!)
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .absolutePadding(left = 10.dp, right = 10.dp)
+                            .clip(RoundedCornerShape(15.dp))
+                            .height(300.dp)
+                            .background(color = colorResource(R.color.grey_1))
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .verticalScroll(rememberScrollState())
+                        ) {
+                            Text(GlobalSchema.ytViewerParameters["desc"]!!, Modifier.absolutePadding(left = 10.dp, right = 10.dp), fontSize = 14.sp, lineHeight = 1.em, color = Color.White)
+                        }
+                        // Add other composables inside the Box if needed
+                    }
 
                 }
             }
